8ceab8ebd1d585a06ccfa37eb4fdb401ad30eefc
Sarthak Lohiya <sarthaklohiya72@gmail.com>
2026-04-02 17:33:45 +0530

    summary box by task

--- app.py
+++ app.py
@@ -2220,4 +2220,4 @@ app = _create_app()
 
 if __name__ == '__main__':
     # host='0.0.0.0' allows access from other devices on the network
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=8080)
